62269d1438938cb8f19c7222467a36a0d359a22b
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2023-01-18 13:18:34 +0200

    first

---
+++ src/main/java/com/example/andersen/AndersenApplication.java
@@ -0,0 +1,13 @@
+package com.example.andersen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndersenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndersenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/andersen/AndersenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.andersen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndersenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
