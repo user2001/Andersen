6fcbf3e068e756fbdfbc10e7c01f7f4f38597570
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2023-01-19 12:10:01 +0200

    arraylist implementation

---
+++ src/main/java/com/example/andersen/arraylist/ArrayList.java
@@ -0,0 +1,110 @@
+package com.example.andersen.arraylist;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class ArrayList<T> implements List<T> {
+    private static int DEFAULT_CAPACITY = 10;
+    private Object[] elements;
+    private int size;
+
+    public ArrayList(int initCapacity) {
+        if (initCapacity <= 0) {
+            throw new IllegalArgumentException();
+        }
+        elements = new Object[initCapacity];
+    }
+
+    public ArrayList() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    @Override
+    public void add(T element) {
+        resizeIfNeeded();
+        elements[size] = element;
+        size++;
+    }
+
+    private void resizeIfNeeded() {
+        if (elements.length == size) {
+            Object[] newArray = new Object[elements.length * 2];
+            System.arraycopy(elements, 0, newArray, 0, size);
+            elements = newArray;
+        }
+    }
+
+    @Override
+    public void add(int index, T element) {
+        Objects.checkIndex(index, size);
+        resizeIfNeeded();
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = element;
+        size++;
+
+    }
+
+    @Override
+    public void set(int index, T element) {
+        Objects.checkIndex(index, size);
+        elements[index] = element;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return (T) elements[index];
+    }
+
+    @Override
+    public T getFirst() {
+        return get(0);
+    }
+
+    @Override
+    public T getLast() {
+        return get(size - 1);
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, size);
+        T removedElement = (T) elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        size--;
+        return removedElement;
+    }
+
+    @Override
+    public boolean contains(T element) {
+        for (int i = 0; i < size; i++) {
+            if (element.equals(elements[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public static <T> List<T> of(T... elements) {
+        List<T> list = new ArrayList<>();
+        Stream.of(elements).forEach(list::add);
+        return list;
+    }
+}
+
