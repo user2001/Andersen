a5d0416b571b60cdbd857b0a652200cf66f44d11
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2023-01-19 10:14:16 +0200

    lombok and default field added

--- src/main/java/com/example/andersen/builder_pattern/Pizza.java
+++ src/main/java/com/example/andersen/builder_pattern/Pizza.java
@@ -1,34 +1,12 @@
 package com.example.andersen.builder_pattern;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class Pizza {
-    private String cheese;
-    private String meat;
-    private String seafood;
-    private String vegetable;
-    private String mushroom;
-
-    private Pizza() {
-    }
-
-    public String getCheese() {
-        return cheese;
-    }
-
-    public String getMeat() {
-        return meat;
-    }
-
-    public String getSeafood() {
-        return seafood;
-    }
-
-    public String getVegetable() {
-        return vegetable;
-    }
-
-    public String getMushroom() {
-        return mushroom;
-    }
+    private String ingredient;
 
     public static PizzaBuilder base() {
         return new PizzaBuilder();
@@ -45,28 +23,8 @@ public class Pizza {
             this.pizza = pizza;
         }
 
-        public PizzaBuilder addCheese(String cheese) {
-            pizza.cheese = cheese;
-            return this;
-        }
-
-        public PizzaBuilder addMeat(String meat) {
-            pizza.meat = meat;
-            return this;
-        }
-
-        public PizzaBuilder addSeafood(String seafood) {
-            pizza.seafood = seafood;
-            return this;
-        }
-
-        public PizzaBuilder addVegetable(String vegetable) {
-            pizza.vegetable = vegetable;
-            return this;
-        }
-
-        public PizzaBuilder addMushroom(String mushroom) {
-            pizza.mushroom = mushroom;
+        public PizzaBuilder addIngredient(String ingredient) {
+            pizza.ingredient = ingredient;
             return this;
         }
 
@@ -81,9 +39,9 @@ class Oven {
     public static Pizza cook() {
 
         return Pizza.base()
-                .addCheese("cheese")
-                .addMeat("meat")
-                .addVegetable("vegetable")
+                .addIngredient("cheese")
+                .addIngredient("tomato")
+                .addIngredient("meat")
                 .build();
     }
 }
