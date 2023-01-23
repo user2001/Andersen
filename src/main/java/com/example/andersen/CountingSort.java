e6ad36cf2aedf13af0ca5f0e209b5c08f94fd810
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2023-01-23 15:04:28 +0200

    counting sort anothe implementation

--- src/main/java/com/example/andersen/CountingSort.java
+++ src/main/java/com/example/andersen/CountingSort.java
@@ -1,26 +1,22 @@
 package com.example.andersen;
 
 public class CountingSort {
-    void sort(char arr[]) {
-
-        char output[] = new char[arr.length];
-
-        int count[] = new int[256];
-        for (int i = 0; i < 256; ++i)
-            count[i] = 0;
-
-        for (int i = 0; i < arr.length; ++i)
-            ++count[arr[i]];
-
-        for (int i = 1; i <= 255; ++i)
-            count[i] += count[i - 1];
-
-        for (int i = arr.length - 1; i >= 0; i--) {
-            output[count[arr[i]] - 1] = arr[i];
-            --count[arr[i]];
+    public static int[] countingSort(int[] theArray, int maxValue) {
+        int numCounts[] = new int[maxValue + 1];
+        for (int num : theArray) {
+            numCounts[num]++;
         }
+        int[] sortedArray = new int[theArray.length];
+        int currentSortedIndex = 0;
 
-        for (int i = 0; i < arr.length; ++i)
-            arr[i] = output[i];
+        for (int n = 0; n < numCounts.length; n++) {
+            int count = numCounts[n];
+            for (int k = 0; k < count; k++) {
+                sortedArray[currentSortedIndex] = n;
+                currentSortedIndex++;
+            }
+        }
+        return sortedArray;
     }
 }
+
